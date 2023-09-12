b8043929a8233385f677f7fbd1344a20dda85d5f
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-09-12 21:12:37 +0530

    added

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <p className='text-3xl text-zinc-700/50 font-medium'>Overview</p>
+      <div className='flex'>
+        <div className='shadow-lg h-32 w-64 p-3 rounded-md'>
+           <div className='flex '>
+               <div className=' mr-4 pr-2 pl-2 pt-2  mt-5 rounded-full bg-green-300'><span class="material-symbols-outlined " style={{fontSize:"30px"}}  >
+                    group
+                     </span></div>
+             <div className='pt-4'><p className='text-2xl font-sans'>164</p>
+        
+                  <p className='font-sans text-xs font-medium text-gray-600'>Total Number of sudents</p></div>
+            </div>
+                 <p className='text-right mt-3 text-xs text-red-800 '>View</p>
+        </div>
+
+      </div>
+
+
+      
+    </div>
   )
 }
 
