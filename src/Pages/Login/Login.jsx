e8217d895d3922d63bce2a5f3a96315744952276
Rohan Ghadge <113715810+Rohan1366@users.noreply.github.com>
2023-09-12 18:36:31 +0530

    added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Login to your account
           </h2>
         </div>
 
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './Register.css'
 import { useState } from 'react'
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
-  
+  const navigate=useNavigate()
   const handleSubmit = (e) => {
     e.preventDefault();
     if (email === "") {
